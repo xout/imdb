5fd6c281a20e54fff065e9e458345032b27a0d6b
FASTxou <fastxout@gmail.com>
2023-02-23 02:53:13 +0900

    add movie info

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import { FiThumbsUp } from 'react-icons/fi';
 export default function Card({result}) {
     return (
         <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
-            <Link href={`/movie/{result.id}`}>
+            <Link href={`/movie/${result.id}`}>
                 <Image 
                     placeholder="blur" 
                     blurDataURL="/spinner.svg" 
