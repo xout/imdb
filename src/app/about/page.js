6d6adc6957964be798c4c17899c3b9ad1699ec8b
FASTxou <fastxout@gmail.com>
2023-02-22 15:49:12 +0900

    about page

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+        <div className="max-w-6xl mx-auto space-x-4">
+            <h1 className="text-2xl font-medium text-amber-600">About</h1>
+            <p>
+                Welcome to bbbbbbbbbbbbbbbbbbbbbbbbb
+            </p>
+        </div>
+        
+    )
+}
