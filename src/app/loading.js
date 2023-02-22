112202d521e1d391ba5abed92b82d15755c90c0b
FASTxou <fastxout@gmail.com>
2023-02-23 00:07:07 +0900

    test

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center">a
             <img className="h-96" src="spinner.svg" alt="loading..." />
         </div>
     )
